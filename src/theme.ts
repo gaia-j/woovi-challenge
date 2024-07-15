e814dc3f6ce686c646bfe93ae2fe8dfc3dd05a79
Hugo Gaia <hugogaia65@gmail.com>
2024-07-15 12:00:13 -0300

    feat: update theme with colors

--- src/theme.ts
+++ src/theme.ts
@@ -1,32 +1,41 @@
 import {createTheme} from "@mui/material/styles";
 
+
+export const colors = {
+  mainGreen: '#03D69D',
+  mainBlue: '#133A6F',
+  darkGray: '#4D4D4D',
+  lightGray: '#E5E5E5',
+  midGray: '#AFAFAF',
+  highlightGreen: '#F4FBF9',
+  white: '#FFFFFF'
+}
+
 export const theme = createTheme({
   palette: {
     primary: {
-      main: '#133A6F',
+      main: colors.mainBlue,
     },
     secondary: {
-      main: '#03D69D',
+      main: colors.mainGreen,
     },
   },
   typography: {
     fontFamily: 'Nunito, Arial, sans-serif',
   },
   components: {
-
     MuiOutlinedInput: {
-
       styleOverrides: {
         root: {
           '&:hover .MuiOutlinedInput-notchedOutline': {
-            borderColor: '#E5E5E5',
+            borderColor: colors.lightGray,
           },
           '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-            borderColor: '#03D69D',
+            borderColor: colors.mainGreen,
           },
         },
         notchedOutline: {
-          borderColor: "#E5E5E5",
+          borderColor: colors.lightGray,
           borderWidth: "2px",
         }
       },
@@ -35,6 +44,7 @@ export const theme = createTheme({
       styleOverrides: {
         input: {
           textAlign: "left",
+          fontWeight: "600"
         },
       },
     },
