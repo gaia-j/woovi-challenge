dc0193a6d21fafa0fbbf2e7c027d4dbdba4d21a4
Hugo Gaia <hugogaia65@gmail.com>
2024-07-16 17:58:59 -0300

    feat: remove header and footer when not in payment routes

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -6,6 +6,11 @@ import {styles} from "./footer.style";
 
 
 export default function Footer(){
+  const paymentRoutes = ['/select-terms', '/payment-card', 'payment-pix']
+  const pathname = window.location.pathname
+  if (!paymentRoutes.includes(pathname)) {
+    return null
+  }
   return (
     <div style={styles.container}>
       <GppGoodOutlinedIcon style={{}}/>
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,7 +3,13 @@ import { ReactComponent as Logo} from "../../assets/logo.svg";
 import React from "react";
 import { styles } from "./header.style";
 
+
 export default function Header() {
+  const paymentRoutes = ['/select-terms', '/payment-card', 'payment-pix']
+  const pathname = window.location.pathname
+  if (!paymentRoutes.includes(pathname)) {
+    return null
+  }
   return (
     <div style={styles.container}>
       <Logo/>
