d9d44d8011b7179eca7579b80bcad04ac5e7b245
Hugo Gaia <hugogaia65@gmail.com>
2024-07-16 18:36:38 -0300

    feat: stop styling input from here

--- src/theme.ts
+++ src/theme.ts
@@ -23,31 +23,6 @@ export const theme = createTheme({
     fontFamily: 'Nunito, Arial, sans-serif',
   },
   components: {
-    MuiOutlinedInput: {
-      styleOverrides: {
-        root: {
-          '&:hover .MuiOutlinedInput-notchedOutline': {
-            borderColor: colors.lightGray,
-          },
-          '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-            borderColor: colors.mainGreen,
-          },
-        },
-        notchedOutline: {
-          borderColor: colors.lightGray,
-          borderWidth: "2px",
-          borderRadius: "8px"
-        }
-      },
-    },
-    MuiInputBase: {
-      styleOverrides: {
-        input: {
-          textAlign: "left",
-          fontWeight: "600"
-        },
-      },
-    },
     MuiAccordionSummary:{
       styleOverrides: {
         root: {
@@ -56,56 +31,5 @@ export const theme = createTheme({
       }
     }
 
-    // MuiStepIcon: {
-    //   styleOverrides: {
-    //     root: {
-    //       color: colors.white,
-    //       border: `2px solid ${colors.lightGray}`,
-    //       borderRadius: "50%",
-    //       width: 14,
-    //       height: 14,
-    //       '&.Mui-completed': {
-    //         color: colors.mainGreen,
-    //         borderColor: colors.mainGreen
-    //       },
-    //       '&.Mui-active': {
-    //         color: colors.white,
-    //         borderColor: colors.mainGreen
-    //       }
-    //     },
-    //     text: {
-    //       display: "none"
-    //     }
-    //   }
-    // },
-    // MuiStepper:{
-    //   styleOverrides:{
-    //     root: {
-    //       position: "relative",
-    //       width: "100%"
-    //     }
-    //   }
-    // },
-    // MuiStepConnector:{
-    //   styleOverrides: {
-    //     root: {
-    //       position: "absolute",
-    //       top: "41px",
-    //       left: "-4px",
-    //       height: "46px",
-    //       '&.Mui-active .MuiStepConnector-line': {
-    //         borderColor: colors.mainGreen
-    //       },
-    //       '&.Mui-completed .MuiStepConnector-line': {
-    //         borderColor: colors.mainGreen
-    //       },
-    //     },
-    //     line: {
-    //       borderWidth: "2px",
-    //       borderColor: colors.lightGray,
-    //       height:"100%"
-    //     },
-    //   }
-    // }
   },
 });
